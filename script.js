d7e547cec7fd60a63265cfce273ca709e6b7548d
pavithiraa <87646281+pavithiraa@users.noreply.github.com>
2021-12-31 19:15:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(()=>{
+    document.getElementById('hh').innerHTML=10;
+   setTimeout(()=>{
+      document.getElementById('hh').innerHTML=9;
+      setTimeout(()=>{
+        document.getElementById('hh').innerHTML=8;
+        setTimeout(()=>{
+            document.getElementById('hh').innerHTML=7;
+            setTimeout(()=>{
+                document.getElementById('hh').innerHTML=6;
+                setTimeout(()=>{
+                    document.getElementById('hh').innerHTML=5;
+                    setTimeout(()=>{
+                        document.getElementById('hh').innerHTML=4;
+                        setTimeout(()=>{
+                            document.getElementById('hh').innerHTML=3;
+                            setTimeout(()=>{
+                                document.getElementById('hh').innerHTML=2;
+                                setTimeout(()=>{
+                                    document.getElementById('hh').innerHTML=1;
+                                    setTimeout(()=>{
+                                        document.getElementById('hh').innerHTML='74 th Happy Independence day!';
+                                      },1000);
+                                  },1000);
+                              },1000);
+                          },1000);
+                      },1000);
+                  },1000);
+              },1000);
+          },1000);
+      },1000);
+   },1000)
+},1000);
+
